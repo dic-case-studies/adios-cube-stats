9ed38a94207c6e2b156535f01e7f581d434f3b6e
maneesh.sutar <maneesh.sutar@thoughtworks.com>
2023-01-03 16:57:49 +0530

    Maneesh / Maniaddhanki : Ran benchmark using bench.sh, stored execution time in time.txt

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,3 +1,6 @@
+rm time.txt
+touch time.txt
+
 # imstat.c
 # start time
 startTime=`gdate +%s%3N`;
@@ -7,8 +10,7 @@ endTime=`gdate +%s%3N`;
 
 diffMilliSeconds="$(($endTime-$startTime))"
 
-echo >> imstat-result.txt 
-echo "Time elapsed : $diffMilliSeconds ms" >> imstat-result.txt
+echo "Time elapsed imstat.c : $diffMilliSeconds ms" >> time.txt
 
 # imstat_adios.cpp
 # start time
@@ -20,7 +22,19 @@ endTime=`gdate +%s%3N`;
 diffMilliSeconds="$(($endTime-$startTime))"
 
 echo >> imstat_adios-result.txt 
-echo "Time elapsed : $diffMilliSeconds ms" >> imstat_adios-result.txt
+echo "Time elapsed imstat_adios.cpp : $diffMilliSeconds ms" >> time.txt
+
+# imstat_adios_single_read.cpp
+# start time
+startTime=`gdate +%s%3N`;
+./build/imstat_adios_single_read.out > imstat_adios_single_read-result.txt 
+#end time
+endTime=`gdate +%s%3N`;
+
+diffMilliSeconds="$(($endTime-$startTime))"
+
+echo >> imstat_adios_single_read-result.txt 
+echo "Time elapsed imstat_adios_single_read.cpp : $diffMilliSeconds ms" >> time.txt
 
 # imstat_adios_mpi.cpp
 # start time
@@ -32,4 +46,16 @@ endTime=`gdate +%s%3N`;
 diffMilliSeconds="$(($endTime-$startTime))"
 
 echo >> imstat_adios_mpi-result.txt 
-echo "Time elapsed : $diffMilliSeconds ms" >> imstat_adios_mpi-result.txt
+echo "Time elapsed imstat_adios_mpi.cpp with n=5 : $diffMilliSeconds ms" >> time.txt
+
+# imstat_adios_mpi_less_reads.cpp
+# start time
+startTime=`gdate +%s%3N`;
+mpiexec -n 5 build/imstat_adios_mpi_less_reads.out > imstat_adios_mpi_less_reads-result.txt 
+#end time
+endTime=`gdate +%s%3N`;
+
+diffMilliSeconds="$(($endTime-$startTime))"
+
+echo >> imstat_adios_mpi_less_reads-result.txt 
+echo "Time elapsed imstat_adios_mpi_less_reads.cpp with n=5 : $diffMilliSeconds ms" >> time.txt
