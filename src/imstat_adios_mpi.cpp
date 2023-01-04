abddc1e189394f11cea2a0f9d81af8006b47292f
maneesh.sutar <maneesh.sutar@thoughtworks.com>
2023-01-04 15:22:49 +0530

    Maneesh / Ravinder: Reading in File random access mode for imstat_adios mpi programs

--- src/imstat_adios_mpi.cpp
+++ src/imstat_adios_mpi.cpp
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <mpi.h>
 
 #include <adios2.h>
 
@@ -18,92 +19,71 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    adios2::fstream in("casa.bp", adios2::fstream::in,
-                       MPI_COMM_WORLD);
+    adios2::fstream inStream("casa.bp", adios2::fstream::in,
+                             MPI_COMM_WORLD);
 
-    adios2::fstep inStep;
+    // get image count of dimensions by reading the BP file variables
+    const std::vector<int64_t> s_numAxis = inStream.read<int64_t>("NAXIS");
+    naxis = s_numAxis.front();
 
-    while (adios2::getstep(in, inStep))
+    // get image dimensions by reading the BP file variables
+    for (int i = 1; i <= naxis; i++)
     {
-        const std::size_t currentStep = inStep.current_step();
-        // get image count of dimensions by reading the BP file variables
-        const std::vector<int64_t> s_numAxis = inStep.read<int64_t>("NAXIS");
-
-        if (rank == 0)
-        {
-            std::cout << "Found numAxis:  " << s_numAxis.front() << " in currentStep " << currentStep << std::endl;
-        }
-
-        naxis = s_numAxis.front();
-
-        // get image dimensions by reading the BP file variables
-        for (int i = 1; i <= naxis; i++)
-        {
-            std::string search_var = "NAXIS" + std::to_string(i);
-            const std::vector<int64_t> s_axis = inStep.read<int64_t>(search_var);
-
-            if (rank == 0)
-            {
-                std::cout << "Found NAXIS" << std::to_string(i) << " :  " << s_axis.front() << " in currentStep "
-                          << currentStep << std::endl;
-            }
-
-            naxes[i - 1] = s_axis.front();
-        }
-        if (rank == 0)
-            std::cout << std::endl;
-
-        size_t spat_size = naxes[0] * naxes[1];
-
-        if (rank == 0)
-        {
-            printf("#%7s %15s %10s %10s %10s %10s %10s %10s %10s\n", "Channel",
-                   "Frequency", "Mean", "Std", "Median", "MADFM", "1%ile", "Min",
-                   "Max");
-            printf("#%7s %15s %10s %10s %10s %10s %10s %10s %10s\n", " ", "MHz",
-                   "mJy/beam", "mJy/beam", "mJy/beam", "mJy/beam", "mJy/beam",
-                   "mJy/beam", "mJy/beam");
-        }
-
-        /* process image one channel at a time; increment channel # in each loop */
-        for (int channel = 0; channel < naxes[2]; channel++)
-        {
-            if (channel % size == rank)
-            {
-                const adios2::Dims start = {static_cast<std::size_t>(0), static_cast<std::size_t>(channel), static_cast<std::size_t>(0), static_cast<std::size_t>(0)};
-
-                const adios2::Dims count = {static_cast<std::size_t>(1), static_cast<std::size_t>(1), static_cast<std::size_t>(naxes[0]), static_cast<std::size_t>(naxes[1])};
-
-                const std::vector<float> data = inStep.read<float>("data", start, count);
-
-                float sum = 0., meanval = 0., minval = 1.E33, maxval = -1.E33;
-                float valid_pix = 0;
-
-                for (size_t ii = 0; ii < spat_size; ii++)
-                {
-                    float val = data[ii];
-                    valid_pix += isnan(val) ? 0 : 1;
-                    val = isnan(val) ? 0.0 : val;
-
-                    sum += val; /* accumlate sum */
-                    if (val < minval)
-                        minval = val; /* find min and  */
-                    if (val > maxval)
-                        maxval = val; /* max values    */
-                }
-                meanval = sum / valid_pix;
-
-                meanval *= 1000.0;
-                minval *= 1000.0;
-                maxval *= 1000.0;
-
-                printf("%8d %15.6f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f\n",
-                       channel+1, 1.0f, meanval, 0.0f, 0.0f, 0.0f, 0.0f, minval, maxval);
-            }
-        }
-        in.end_step();
+        std::string search_var = "NAXIS" + std::to_string(i);
+        const std::vector<int64_t> s_axis = inStream.read<int64_t>(search_var);
+        naxes[i - 1] = s_axis.front();
     }
-    in.close();
+
+    size_t spat_size = naxes[0] * naxes[1];
+
+    if (rank == 0)
+    {
+        printf("#%7s %15s %10s %10s %10s %10s %10s %10s %10s\n", "Channel",
+               "Frequency", "Mean", "Std", "Median", "MADFM", "1%ile", "Min",
+               "Max");
+        printf("#%7s %15s %10s %10s %10s %10s %10s %10s %10s\n", " ", "MHz",
+               "mJy/beam", "mJy/beam", "mJy/beam", "mJy/beam", "mJy/beam",
+               "mJy/beam", "mJy/beam");
+    }
+
+    /* process image one channel at a time; increment channel # in each loop */
+    for (int channel = 0; channel < naxes[2]; channel++)
+    {
+        if (channel % size == rank)
+        {
+            const adios2::Dims start = {static_cast<std::size_t>(0), static_cast<std::size_t>(channel), static_cast<std::size_t>(0), static_cast<std::size_t>(0)};
+
+            const adios2::Dims count = {static_cast<std::size_t>(1), static_cast<std::size_t>(1), static_cast<std::size_t>(naxes[0]), static_cast<std::size_t>(naxes[1])};
+
+            const std::vector<float> data = inStream.read<float>("data", start, count);
+
+            float sum = 0., meanval = 0., minval = 1.E33, maxval = -1.E33;
+            float valid_pix = 0;
+
+            for (size_t ii = 0; ii < spat_size; ii++)
+            {
+                float val = data[ii];
+                valid_pix += isnan(val) ? 0 : 1;
+                val = isnan(val) ? 0.0 : val;
+
+                sum += val; /* accumlate sum */
+                if (val < minval)
+                    minval = val; /* find min and  */
+                if (val > maxval)
+                    maxval = val; /* max values    */
+            }
+            meanval = sum / valid_pix;
+
+            meanval *= 1000.0;
+            minval *= 1000.0;
+            maxval *= 1000.0;
+
+            printf("%8d %15.6f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f\n",
+                   channel + 1, 1.0f, meanval, 0.0f, 0.0f, 0.0f, 0.0f, minval, maxval);
+        }
+    }
+
+    inStream.close();
 
     MPI_Finalize();
     return 0;
--- src/imstat_adios_mpi_less_reads.cpp
+++ src/imstat_adios_mpi_less_reads.cpp
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <mpi.h>
 
 #include <adios2.h>
 
@@ -18,103 +19,83 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    adios2::fstream in("casa.bp", adios2::fstream::in,
-                       MPI_COMM_WORLD);
+    adios2::fstream inStream("casa.bp", adios2::fstream::in,
+                             MPI_COMM_WORLD);
 
-    adios2::fstep inStep;
+    // get image count of dimensions by reading the BP file variables
+    const std::vector<int64_t> s_numAxis = inStream.read<int64_t>("NAXIS");
+    naxis = s_numAxis.front();
 
-    while (adios2::getstep(in, inStep))
+    // get image dimensions by reading the BP file variables
+    for (int i = 1; i <= naxis; i++)
     {
-        const std::size_t currentStep = inStep.current_step();
-        // get image count of dimensions by reading the BP file variables
-        const std::vector<int64_t> s_numAxis = inStep.read<int64_t>("NAXIS");
+        std::string search_var = "NAXIS" + std::to_string(i);
+        const std::vector<int64_t> s_axis = inStream.read<int64_t>(search_var);
 
-        if (rank == 0)
-        {
-            std::cout << "Found numAxis:  " << s_numAxis.front() << " in currentStep " << currentStep << std::endl;
-        }
-
-        naxis = s_numAxis.front();
-
-        // get image dimensions by reading the BP file variables
-        for (int i = 1; i <= naxis; i++)
-        {
-            std::string search_var = "NAXIS" + std::to_string(i);
-            const std::vector<int64_t> s_axis = inStep.read<int64_t>(search_var);
-
-            if (rank == 0)
-            {
-                std::cout << "Found NAXIS" << std::to_string(i) << " :  " << s_axis.front() << " in currentStep "
-                          << currentStep << std::endl;
-            }
-
-            naxes[i - 1] = s_axis.front();
-        }
-        if (rank == 0)
-            std::cout << std::endl;
-
-        size_t spat_size = naxes[0] * naxes[1];
-
-        if (rank == 0)
-        {
-            printf("#%7s %15s %10s %10s %10s %10s %10s %10s %10s\n", "Channel",
-                   "Frequency", "Mean", "Std", "Median", "MADFM", "1%ile", "Min",
-                   "Max");
-            printf("#%7s %15s %10s %10s %10s %10s %10s %10s %10s\n", " ", "MHz",
-                   "mJy/beam", "mJy/beam", "mJy/beam", "mJy/beam", "mJy/beam",
-                   "mJy/beam", "mJy/beam");
-        }
-
-        int quot = naxes[2] / size;
-        int channelsToRead = quot;
-        int startOffset = 0;
-        int rem = naxes[2] % size;
-        if (rank >= size - rem)
-        {
-            channelsToRead = channelsToRead + 1;
-            startOffset = rem - (size - rank);
-        }
-        int startChannel = rank * quot + startOffset;
-
-        const adios2::Dims start = {static_cast<std::size_t>(0), static_cast<std::size_t>(startChannel), static_cast<std::size_t>(0), static_cast<std::size_t>(0)};
-
-        const adios2::Dims count = {static_cast<std::size_t>(1), static_cast<std::size_t>(channelsToRead), static_cast<std::size_t>(naxes[0]), static_cast<std::size_t>(naxes[1])};
-
-        // std::cout << "Rank: " << rank << "startChannel: " << startChannel << "channelsToRead: " << channelsToRead << std::endl; 
-
-        const std::vector<float> data = inStep.read<float>("data", start, count);
-
-        /* process image one channel at a time; increment channel # in each loop */
-        for (int channel = 0; channel < channelsToRead; channel++)
-        {
-
-            float sum = 0., meanval = 0., minval = 1.E33, maxval = -1.E33;
-            float valid_pix = 0;
-
-            for (size_t ii = channel * spat_size; ii < (channel + 1) * spat_size; ii++)
-            {
-                float val = data[ii];
-                valid_pix += isnan(val) ? 0 : 1;
-                val = isnan(val) ? 0.0 : val;
-
-                sum += val; /* accumlate sum */
-                if (val < minval)
-                    minval = val; /* find min and  */
-                if (val > maxval)
-                    maxval = val; /* max values    */
-            }
-            meanval = sum / valid_pix;
-
-            meanval *= 1000.0;
-            minval *= 1000.0;
-            maxval *= 1000.0;
-
-            printf("%8d %15.6f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f\n",
-                   startChannel + channel +1, 1.0f, meanval, 0.0f, 0.0f, 0.0f, 0.0f, minval, maxval);
-        }
-        in.end_step();
+        naxes[i - 1] = s_axis.front();
     }
-    in.close();
+
+    size_t spat_size = naxes[0] * naxes[1];
+
+    if (rank == 0)
+    {
+        printf("#%7s %15s %10s %10s %10s %10s %10s %10s %10s\n", "Channel",
+               "Frequency", "Mean", "Std", "Median", "MADFM", "1%ile", "Min",
+               "Max");
+        printf("#%7s %15s %10s %10s %10s %10s %10s %10s %10s\n", " ", "MHz",
+               "mJy/beam", "mJy/beam", "mJy/beam", "mJy/beam", "mJy/beam",
+               "mJy/beam", "mJy/beam");
+    }
+
+    int quot = naxes[2] / size;
+    int channelsToRead = quot;
+    int startOffset = 0;
+    int rem = naxes[2] % size;
+    if (rank >= size - rem)
+    {
+        channelsToRead = channelsToRead + 1;
+        startOffset = rem - (size - rank);
+    }
+    int startChannel = rank * quot + startOffset;
+
+    const adios2::Dims start = {static_cast<std::size_t>(0), static_cast<std::size_t>(startChannel), static_cast<std::size_t>(0), static_cast<std::size_t>(0)};
+
+    const adios2::Dims count = {static_cast<std::size_t>(1), static_cast<std::size_t>(channelsToRead), static_cast<std::size_t>(naxes[0]), static_cast<std::size_t>(naxes[1])};
+
+    // std::cout << "Rank: " << rank << "startChannel: " << startChannel << "channelsToRead: " << channelsToRead << std::endl;
+
+    const std::vector<float> data = inStream.read<float>("data", start, count);
+
+    /* process image one channel at a time; increment channel # in each loop */
+    for (int channel = 0; channel < channelsToRead; channel++)
+    {
+
+        float sum = 0., meanval = 0., minval = 1.E33, maxval = -1.E33;
+        float valid_pix = 0;
+
+        for (size_t ii = channel * spat_size; ii < (channel + 1) * spat_size; ii++)
+        {
+            float val = data[ii];
+            valid_pix += isnan(val) ? 0 : 1;
+            val = isnan(val) ? 0.0 : val;
+
+            sum += val; /* accumlate sum */
+            if (val < minval)
+                minval = val; /* find min and  */
+            if (val > maxval)
+                maxval = val; /* max values    */
+        }
+        meanval = sum / valid_pix;
+
+        meanval *= 1000.0;
+        minval *= 1000.0;
+        maxval *= 1000.0;
+
+        printf("%8d %15.6f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f %10.3f\n",
+               startChannel + channel + 1, 1.0f, meanval, 0.0f, 0.0f, 0.0f, 0.0f, minval, maxval);
+    }
+
+    inStream.close();
 
     MPI_Finalize();
     return 0;
